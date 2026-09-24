09ba1f918cc8c1aae939216a553d1b460dbc5a97
Sergey Ilinykh <ilinyh.s@gmail.com>
2026-09-24 14:51:45 +0700

    fix for erasableSyntaxOnly enabled

--- src/model/duration-precision-format.ts
+++ src/model/duration-precision-format.ts
@@ -1,7 +1,10 @@
-export enum DurationPrecisionFormat {
-  days = 'days',
-  hours = 'hours',
-  minutes = 'minutes',
-  seconds = 'seconds',
-  milliseconds = 'milliseconds'
-}
+export const DurationPrecisionFormat = Object.freeze({
+  days: 'days',
+  hours: 'hours',
+  minutes: 'minutes',
+  seconds: 'seconds',
+  milliseconds: 'milliseconds'
+} as const);
+
+export type DurationPrecisionFormat =
+  typeof DurationPrecisionFormat[keyof typeof DurationPrecisionFormat];
