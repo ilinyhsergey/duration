0b5b0b6afc4a46e22a82bb977063d04a287bfc3c
Sergey Ilinykh <ilinyh.s@gmail.com>
2026-09-24 15:02:21 +0700

    imports

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
+export { Duration } from './duration.ts';
 export { reformatTime } from './format/time-formatter.ts';
+export {
+  durationPrecisionFormatOrder, durationPrecisionFormatToDurationKey, durationPrecisionFormatPostfix,
+} from './format/duration-precision-format.constants.ts';
+export { type DurationConfig } from './model/duration-config.ts';
+export { DurationPrecisionFormat } from './model/duration-precision-format.ts';
